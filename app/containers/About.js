096bf661073a43bf13948a10d17fed2feae84b5d
chenbin <ewolfok@126.com>
2016-08-03 10:27:53 +0800

    practive 'instance property', 'instance method', 'class property (static)', 'class method (static)', 'getter/setter';

--- app/containers/About.js
+++ app/containers/About.js
@@ -1,17 +1,49 @@
 import React, {Component, PropTypes} from 'react';
 
 class About extends Component {
+    constructor(props) {
+        super(props)
+
+        this.i = 'init info'
+        this.handleClick = this.handleClick.bind(this)
+    }
+
     render() {
         return (
-            <div>
-                About pages
+            <div onClick={this.handleClick}>
+                About pages {this.info}
             </div>
         );
     }
 
     test() {
-        console.log('called with ref test!')
+        console.log('called with ref test!', this.info)
     }
+
+    handleClick(e) {
+        this.info = this.i + '_'
+        console.log(About.count, this.i, this.info)
+        About.staticMethod()
+        About.staticMethodIn()
+    }
+
+    static staticMethodIn() {
+        console.assert(this === About)
+        console.log(this.count++)
+    }
+    get info() {
+        return 'hi.' + this.i
+    }
+
+    set info(i) {
+        this.i = i
+    }
+}
+
+About.count = 10
+About.staticMethod = function() {
+    console.assert(this === About)
+    console.log(this.count++)
 }
 
 About.propTypes = {
